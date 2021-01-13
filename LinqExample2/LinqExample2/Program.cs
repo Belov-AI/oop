f0274772d638ecdd2fc28e36523a0f3cc3606239
Belov-AI <Belov.Alexander@urfu.ru>
2021-01-13 19:15:34 +0500

    Добавлен проект LinqExample2

---
+++ LinqExample2/LinqExample2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { -789, 23, 0, 456, 1111, 798 };
+
+            Console.WriteLine(GetMaxDigitsSumNumber(array));
+            Console.WriteLine(GetMaxDigitsSumNumber2(array));
+
+            Console.WriteLine(GetMaxDigitsSumNumber(new int[0]));
+
+            Console.ReadKey();
+        }
+
+        static int GetMaxDigitsSumNumber(int[] numbers)
+        {
+            return numbers
+                .OrderByDescending(x => Math.Abs(x)
+                    .ToString()
+                    .Aggregate(0, (s, d) => s + int.Parse(d.ToString())))
+                .ThenByDescending(x => x)
+                .FirstOrDefault();
+        }
+
+        static int GetMaxDigitsSumNumber2(int[] array)
+        {
+            return array
+            .Select(x => (
+                Math.Abs(x)
+                .ToString()
+                .Select(d => int.Parse(d.ToString()))
+                .Aggregate(0, (s, y) => s + y), 
+                x)
+                )
+            .Max()
+            .Item2;
+        }
+    }
+}
